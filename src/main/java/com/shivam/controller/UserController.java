9b630e4a2a277ae8fa5922a8636ed4874a71b7af
Shivam Aggarwal <shivam01101995@gmail.com>
2020-09-11 15:55:08 +0530

    UserController + homepage.html

--- src/main/java/com/shivam/controller/UserController.java
+++ src/main/java/com/shivam/controller/UserController.java
@@ -1,12 +1,18 @@
 package com.shivam.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
 
-	
+	@GetMapping("/homepage")
+	public String homepage(Model model) {
+		model.addAttribute("date", new java.util.Date());
+		return "homepage";
+	}
 	
 }
