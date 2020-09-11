a33c473d28b34b6a79fa07836985cd9d00d2468b
Shivam Aggarwal <shivam01101995@gmail.com>
2020-09-11 13:51:32 +0530

    Add Thymeleaf dependency

---
+++ src/main/java/com/shivam/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.shivam.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+	
+	
+}
