4a41c62e40117e11df374606437e7291ef6e92b8
Shivam Aggarwal <shivam01101995@gmail.com>
2020-09-11 18:29:06 +0530

    User registration page

--- src/main/java/com/shivam/controller/UserController.java
+++ src/main/java/com/shivam/controller/UserController.java
@@ -1,18 +1,39 @@
 package com.shivam.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.shivam.Entity.User;
+import com.shivam.Service.UserService;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
 
+	@Autowired
+	UserService userService;
+	
 	@GetMapping("/homepage")
 	public String homepage(Model model) {
 		model.addAttribute("date", new java.util.Date());
 		return "homepage";
+	}
+	
+	@GetMapping("/register")
+	public String register(Model model) {
+		model.addAttribute("userData", new User());
+		return "register-user";
+	}
+	
+	@PostMapping("/saveUser")
+	public String saveUser(@ModelAttribute("userData") User user) {
+		userService.save(user);
+		return "redirect:/users/homepage";
 	}
 	
 }
