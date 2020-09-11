2cfad0025952b54ae5afe16fb33571f3fb46b149
Shivam Aggarwal <shivam01101995@gmail.com>
2020-09-11 22:10:47 +0530

    Add url-shortening functionality

--- src/main/java/com/shivam/controller/UserController.java
+++ src/main/java/com/shivam/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.shivam.Entity.User;
 import com.shivam.Service.UserService;
@@ -34,6 +35,34 @@ public class UserController {
 	public String saveUser(@ModelAttribute("userData") User user) {
 		userService.save(user);
 		return "redirect:/users/homepage";
+	}
+	
+	@PostMapping("/login")
+	public String loginUser(Model model, @RequestParam("email") String email, @RequestParam("password") String password) {
+		User user = userService.loginUser(email, password);
+		if(user == null) {
+			model.addAttribute("loginError", "Invalid Credentials, try again");
+			return "homepage";
+		}
+		model.addAttribute("userData", user);
+		return "user-homepage";
+	}
+	
+	
+	@PostMapping("/createShortUrl")
+	public String createShortUrl(Model model, @RequestParam("originalUrl") String originalUrl,
+			 					@RequestParam("userId") int userId) {
+		
+		User user = userService.findById(userId);
+		model.addAttribute("userData", user);
+		
+		if(originalUrl == null || originalUrl.contentEquals("")) {
+			model.addAttribute("urlError", "Empty URL!");
+			return "user-homepage";
+		}
+		
+		
+		return "user-homepage";
 	}
 	
 }
