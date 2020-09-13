285cadef88c132a15e7bbcd21fabd5f1cdb4703b
Shivam Aggarwal <shivam01101995@gmail.com>
2020-09-13 17:34:43 +0530

    Update sql for insert exception and change ddl-auto=update for db retention

--- src/main/resources/myscript.sql
+++ src/main/resources/myscript.sql
@@ -1,2 +1,3 @@
-insert into url_seed(seed_value) values ('AAA');
-insert into user_table values(null,'s','Shivam','Aggarwal',0,'s');
+insert ignore into url_seed values (1, 'AAA');
+insert ignore into user_table values(null,'s','Shivam','Aggarwal',0,'s');
+insert ignore into url_table values(null, '2021-09-13 17:26:04.267', 'www.google.com','www.google.com',1);
