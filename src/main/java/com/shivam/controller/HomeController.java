4ef24aeb3f0333aab1c0ad7a8cb289dcf74a0e93
Shivam Aggarwal <shivam01101995@gmail.com>
2020-09-18 14:12:03 +0530

    Refactored code, added new home controller and moved non-user related mapping to it

--- src/main/java/com/shivam/controller/HomeController.java
+++ src/main/java/com/shivam/controller/HomeController.java
@@ -1,13 +1,28 @@
 package com.shivam.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.shivam.Entity.User;
+import com.shivam.Service.UrlService;
+import com.shivam.Service.UserService;
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	UserService userService;
+	
+	@Autowired
+	UrlService urlService;
 	
 	@GetMapping("/homepage")
 	public String homepage(Model model) {
@@ -19,6 +34,33 @@ public class HomeController {
 	public String register(Model model) {
 		model.addAttribute("userData", new User());
 		return "register-user";
+	}
+	
+	@PostMapping("/saveUser")
+	public String saveUser(Model model, @Valid @ModelAttribute("userData") User user, BindingResult result) {
+		if(user == null || result.hasErrors() == true) {
+			return "register-user";
+		}
+		
+		//Check is someone has already registerd with same email id
+		if(userService.findUserByEmail(user.getEmail()) == true){
+			model.addAttribute("emailError","This email is already registered");
+			return "register-user";
+		}
+		
+		userService.save(user);
+		return "redirect:/homepage";
+	}
+	
+	@PostMapping("/userLogin")
+	public String loginUser(Model model, @RequestParam("email") String email, @RequestParam("password") String password) {
+		User user = userService.loginUser(email, password);
+		if(user == null) {
+			model.addAttribute("loginError", "Invalid Credentials, try again");
+			return "homepage";
+		}
+		model.addAttribute("userData", user);
+		return "user-homepage";
 	}
 	
 }
--- src/main/java/com/shivam/controller/UserController.java
+++ src/main/java/com/shivam/controller/UserController.java
@@ -2,14 +2,9 @@ package com.shivam.controller;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,46 +27,6 @@ public class UserController {
 	
 	@Autowired
 	GlobalUrlMapping mappingService;
-	
-	@GetMapping("/homepage")
-	public String homepage(Model model) {
-		model.addAttribute("date", new java.util.Date());
-		return "homepage";
-	}
-	
-	@GetMapping("/register")
-	public String register(Model model) {
-		model.addAttribute("userData", new User());
-		return "register-user";
-	}
-	
-	@PostMapping("/saveUser")
-	public String saveUser(Model model, @Valid @ModelAttribute("userData") User user, BindingResult result) {
-		if(user == null || result.hasErrors() == true) {
-			return "register-user";
-		}
-		
-		//Check is someone has already registerd with same email id
-		if(userService.findUserByEmail(user.getEmail()) == true){
-			model.addAttribute("emailError","This email is already registered");
-			return "register-user";
-		}
-		
-		userService.save(user);
-		return "redirect:/users/homepage";
-	}
-	
-	@PostMapping("/login")
-	public String loginUser(Model model, @RequestParam("email") String email, @RequestParam("password") String password) {
-		User user = userService.loginUser(email, password);
-		if(user == null) {
-			model.addAttribute("loginError", "Invalid Credentials, try again");
-			return "homepage";
-		}
-		model.addAttribute("userData", user);
-		return "user-homepage";
-	}
-	
 	
 	@PostMapping("/createShortUrl")
 	public String createShortUrl(Model model, @RequestParam("originalUrl") String originalUrl,
